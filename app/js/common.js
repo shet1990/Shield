7927430221add36f635c0b2f6f816e3a77716cae
shaternik1990 <shaternik@seobility.by>
2019-10-15 15:02:22 +0300

    111

--- app/js/common.js
+++ app/js/common.js
@@ -5,6 +5,25 @@ $(function() {
 		$(this).parent().submit();
 	});
 
+	$('.my_hamburger').click(function(){
+		//Появление
+		if(!$(this).hasClass('is-active')){
+			$(this).addClass('is-active');
+			$('header').addClass('is-active');
+		} else {
+			//Скрытие
+			$(this).removeClass('is-active');
+			$('header').removeClass('is-active');
+		}
+	});
+	$(".authUrl").fancybox({
+		fitToView	: false,
+		autoSize	: true,
+		closeClick	: false,
+		openEffect	: 'elastic',
+		closeEffect	: 'elastic'
+	});
+
 
 
 
